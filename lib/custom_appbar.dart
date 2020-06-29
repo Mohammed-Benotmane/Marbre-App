635188527fbb45a77eba1225119e409a4c479c83
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-06-29 20:55:58 +0200

    update custom appbar

--- lib/custom_appbar.dart
+++ lib/custom_appbar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sofamarbre/const.dart';
 
 class CustomAppBar extends StatefulWidget {
   @override
@@ -8,6 +9,83 @@ class CustomAppBar extends StatefulWidget {
 class _CustomAppBarState extends State<CustomAppBar> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Expanded(
+      flex: 1,
+      child: Stack(
+        children: <Widget>[
+          Positioned.fill(
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 25),
+              child: Material(
+                elevation: 7,
+                borderRadius: BorderRadius.circular(25),
+                shadowColor: mainColor,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: mainColor,
+                      borderRadius: BorderRadius.only(
+                          bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(18, 0, 18, 35),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          "Welcome to Marbre",
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 26,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                        CircleAvatar(
+                          radius: 28,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.person,
+                            color: mainColor,
+                            size: 30,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            child: Center(
+              child: Container(
+                child: Material(
+                  borderRadius: BorderRadius.circular(25),
+                  elevation: 7,
+                  shadowColor: mainColor,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.search),
+                      hintText: "Search",
+                      fillColor: Colors.white,
+                      filled: true,
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(25)),
+                      focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                          borderRadius: BorderRadius.circular(25)),
+                    ),
+                  ),
+                ),
+                width: MediaQuery.of(context).size.width * .8,
+              ),
+            ),
+            bottom: 0,
+            left: 0,
+            right: 0,
+          )
+        ],
+      ),
+    );
   }
 }
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart';
 import 'package:sofamarbre/const.dart';
+import 'package:sofamarbre/custom_appbar.dart';
 import 'package:sofamarbre/models/marbre.dart';
 import 'package:sofamarbre/screens/marbre_detail.dart';
 
@@ -18,86 +19,7 @@ class _HomeState extends State<Home> {
   String imageUrl;
   List marbresList = [];
 
-  customAppBar() {
-    return Expanded(
-      flex: 1,
-      child: Stack(
-        children: <Widget>[
-          Positioned.fill(
-            child: Padding(
-              padding: const EdgeInsets.only(bottom: 25),
-              child: Material(
-                elevation: 7,
-                borderRadius: BorderRadius.circular(25),
-                shadowColor: mainColor,
-                child: Container(
-                  decoration: BoxDecoration(
-                      color: mainColor,
-                      borderRadius: BorderRadius.only(
-                          bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(18, 0, 18, 35),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Welcome to Marbre",
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: 26,
-                            color: Colors.white,
-                            fontWeight: FontWeight.w500,
-                          ),
-                        ),
-                        CircleAvatar(
-                          radius: 28,
-                          backgroundColor: Colors.white,
-                          child: Icon(
-                            Icons.person,
-                            color: mainColor,
-                            size: 30,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            child: Center(
-              child: Container(
-                child: Material(
-                  borderRadius: BorderRadius.circular(25),
-                  elevation: 7,
-                  shadowColor: mainColor,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      hintText: "Search",
-                      fillColor: Colors.white,
-                      filled: true,
-                      enabledBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(25)),
-                      focusedBorder: UnderlineInputBorder(
-                          borderSide: BorderSide(color: Colors.white),
-                          borderRadius: BorderRadius.circular(25)),
-                    ),
-                  ),
-                ),
-                width: MediaQuery.of(context).size.width * .8,
-              ),
-            ),
-            bottom: 0,
-            left: 0,
-            right: 0,
-          )
-        ],
-      ),
-    );
-  }
+
 
   Future<void> getMarbre() async {
     try {
@@ -141,7 +63,7 @@ class _HomeState extends State<Home> {
       ),
       body: Column(
         children: <Widget>[
-          customAppBar(),
+          CustomAppBar(),
           Expanded(
             flex: 4,
             child: Padding(
