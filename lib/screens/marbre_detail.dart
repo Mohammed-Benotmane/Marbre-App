b873da8dbdc3b21d6bfba021c7be95abf85f59f6
mohammed-benotmane <mohammedbenotmanetom@gmail.com>
2020-06-30 14:26:02 +0200

    update design

--- lib/screens/marbre_detail.dart
+++ lib/screens/marbre_detail.dart
@@ -17,104 +17,145 @@ class _MarbreDetailState extends State<MarbreDetail> {
     super.initState();
   }
 
+  customAppBar(){
+      return Expanded(
+        flex: 1,
+        child: Stack(
+          children: <Widget>[
+            Positioned.fill(
+              child: Padding(
+                padding: const EdgeInsets.only(bottom: 25),
+                child: Material(
+                  elevation: 7,
+                  borderRadius: BorderRadius.circular(25),
+                  shadowColor: mainColor,
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: mainColor,
+                        borderRadius: BorderRadius.only(
+                            bottomLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(18, 0, 18, 35),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Welcome to Marbre",
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 26,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          CircleAvatar(
+                            radius: 28,
+                            backgroundColor: Colors.white,
+                            child: Icon(
+                              Icons.person,
+                              color: mainColor,
+                              size: 30,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        elevation: 10,
-        centerTitle: true,
+        elevation: 0,
         backgroundColor: mainColor,
-        title: Text("Marbre Detail"),
+        actions: <Widget>[IconButton(onPressed: () {}, icon: Icon(Icons.filter_list))],
       ),
-      body: Padding(
-        padding: const EdgeInsets.only(top: 10, left: 20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          customAppBar(),
+          Expanded(
+            flex: 3,
+            child: Column(
               children: <Widget>[
-                Column(
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
                   children: <Widget>[
-                    FlatButton.icon(
-                      label: Text(""),
-                      onPressed: () {},
-                      icon: Icon(Icons.add,color: mainColor,size: 50,),
+                    Material(
+                      elevation: 10,
+                      shadowColor: mainColor,
+                      borderRadius: BorderRadius.circular(25),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(25),
+                        child: Image.network(
+                          widget.marbre.image,
+                          height: 100,
+
+                        ),
+                      ),
                     ),
-                    SizedBox(height: 20),
-                    Text("${widget.marbre.price}"),
-                    SizedBox(height: 20),
-                    FlatButton.icon(
-                      label: Text(""),
-                      onPressed: () {},
-                      icon: Icon(Icons.minimize,color: mainColor,size: 50,),
-                    ),
+
                   ],
                 ),
-                Material(
-                  elevation: 10,
-                  shadowColor: mainColor,
-                  borderRadius: BorderRadius.circular(25),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(25),
-                    child: Image.network(
-                      widget.marbre.image,
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      height: MediaQuery.of(context).size.height / 1.5,
-                      fit: BoxFit.fitHeight,
-                    ),
+
+                SizedBox(height: 20,),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, right: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        widget.marbre.title,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.w500,
+                          color: Colors.grey.shade800,
+                        ),
+                      ),
+                      Text(
+                        "${widget.marbre.price} DA",
+                        style: TextStyle(
+                          fontSize: 22,
+                          color: mainColor,
+                        ),
+                      ),
+                    ],
                   ),
                 ),
-
               ],
             ),
-            SizedBox(height: 20,),
-            Padding(
-              padding: const EdgeInsets.only(top: 15, right: 15),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    widget.marbre.title,
-                    style: TextStyle(
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
-                      color: Colors.grey.shade800,
-                    ),
-                  ),
-                  Text(
-                    "${widget.marbre.price} DA",
-                    style: TextStyle(
-                      fontSize: 22,
-                      color: mainColor,
-                    ),
-                  ),
-                ],
-              ),
+          ),
+
+          Text(
+            widget.marbre.origin.toUpperCase(),
+            style: TextStyle(
+              fontSize: 16,
+              color: mainColor,
             ),
-            Text(
-              widget.marbre.origin.toUpperCase(),
-              style: TextStyle(
-                fontSize: 16,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(topRight: Radius.circular(25),bottomLeft: Radius.circular(25)),
+              child: RaisedButton(
+                padding: EdgeInsets.fromLTRB(100, 10, 100, 10),
+                onPressed: () {},
                 color: mainColor,
+                child: Text("Validate",style: TextStyle(color: Colors.white,fontSize: 25),),
               ),
             ),
-            SizedBox(
-              height: 30,
-            ),
-            Center(
-              child: ClipRRect(
-                borderRadius: BorderRadius.only(topRight: Radius.circular(25),bottomLeft: Radius.circular(25)),
-                child: RaisedButton(
-                  padding: EdgeInsets.fromLTRB(100, 10, 100, 10),
-                  onPressed: () {},
-                  color: mainColor,
-                  child: Text("Validate",style: TextStyle(color: Colors.white,fontSize: 25),),
-                ),
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
